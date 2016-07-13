62bcccdceb338d7e596b02e7f7c60ee76799d806
Byron Arocha <bjarocha911@gmail.com>
2016-07-13 16:28:25 +0000

    Add edit and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,30 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  
   def new
     @user = User.new
+  end
+  
+  def edit
+    
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
-      #success
       flash[:success] = "Welcome to the Kaiju Blog, #{@user.username}!"
       redirect_to articles_path
     else
-      #failure
       render 'new'
+    end
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully!"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
   
@@ -19,5 +32,9 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
     end
 end
